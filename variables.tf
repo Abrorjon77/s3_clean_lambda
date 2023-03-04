ad1bf0b24672c655bc56d0826dae02edbcbf6780
Abrorjon77 <110739859+Abrorjon77@users.noreply.github.com>
2023-03-04 01:43:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "bucket_name" {
-  default = ""
+  description = "Must be unique aws-wide"
+  type        = string
+  default     = "abror-bucket-project"
 }
 variable "arn_my_sns_topic" {
   default = ""
